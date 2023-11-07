1f962b80f90fca3abeaacce408504cb7b58440d8
3B032104 <3B032104@gm.student.ncut.edu.tw>
2023-11-07 23:20:48 +0800

    Use save() update posts table data

--- routes/web.php
+++ routes/web.php
@@ -57,10 +57,19 @@ Route::get('/', function () {
     dd($posts);
     */
 
+    /*use update() update posts table data
     $post = Post::find(1);
     $post->update([
         'title' => 'updated title',
         'content' => 'updated content'
     ]);
+    */
+
+    //use save() update posts table data
+    $post = Post::find(1);
+    $post -> title = 'saved title';
+    $post -> content = 'saved content';
+    $post -> save();
+
 
 });
