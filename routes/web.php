a774fc464db794d0fd3fe7c6e119004a5f037809
3B032104 <3B032104@gm.student.ncut.edu.tw>
2023-11-07 21:59:30 +0800

    Use create() add new data

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,17 @@ use App\Models\Post;
 
 Route::get('/', function () {
     //return view('welcome');
-
+    /* use save() add new data
     $post = new Post();
     $post -> title = 'test title';
     $post -> content = 'test content';
     $post -> save(); //將新貼文 $post 存入 posts table
     return 'save, ok.';
+    */
+
+    Post::create([
+        'title' => 'created title',
+        'content' => 'created content',
+    ]);
+
 });
